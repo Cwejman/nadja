91192da6c69b8a1b006e6b579421f4775936e2d3
Cwejman <jcwejman@gmail.com>
2016-05-18 17:28:29 +0200

    .

---
+++ README.md
@@ -0,0 +1,11 @@
+# Nadja Gustavsson
+
+A website about Najda Gustavsson and her gardening service. She works in Sweden bohuslän and wants a website were poeplo can understand what type of gardening she does, prices and of course contact.
+
+This site is a bit outdated especially behind the scenes. The goal is to rebuild it in a modern fashion this spring.
+
+## Ingredients
+
+Nadjas site is mostly responsive and uses Jade, SASS and Javascript with JQuery. JQuery covers the hiding of different div for navigation but will be moved to a more suited and faster framework, such as Angular. Jade and SASS is proccesed in development which gives the final site only html and css to work with.
+
+
---
+++ assets/js/main.js
@@ -0,0 +1,72 @@
+var buttons = ['.dd.t','.d.p','.d.k'];
+var buttons_t = ['.d.1','.d.2','.d.3','.d.4'];
+var active = false;
+var active_t = false;
+var current = '';
+var current_t = '';
+
+function show($par, $array, $current){
+  for (var i = 0; i < $array.length; i++) {
+    if ($par == $array[i]) {
+      $($par).show();
+      active = true;
+      current = $array[i];
+    } else {
+      $($array[i]).hide();
+    }
+  }
+
+}
+
+function activate($par, $array){
+  if(!active) {
+    show($par, $array)
+  }else{
+    if ($par == current) {
+      for (var i = 0; i < $array.length; i++) {
+        $($array[i]).hide();
+      }
+      active = false;
+    }else{
+      show($par, $array, current);
+    }
+  }
+
+  $("html, body").scrollTop($(document).height() / 2);
+
+}
+
+function activate_t($par, $array){
+  if(!active) {
+    show($par, $array)
+  }else{
+    if ($par == current_t) {
+      for (var i = 0; i < $array.length; i++) {
+        $($array[i]).hide();
+      }
+      active_t = false;
+    }else{
+      show($par, $array, current_t);
+    }
+  }
+
+  $("html, body").scrollTop($(document).height() / 2);
+
+}
+
+$(document).ready(function(){
+
+  activate("none", buttons);
+  activate_t("none", buttons_t);
+
+  $(".b.h").click(function(){ activate("none", buttons); });
+  $(".b.t").click(function(){ activate(".dd.t", buttons); });
+  $(".b.p").click(function(){ activate(".d.p", buttons); });
+  $(".b.k").click(function(){ activate(".d.k", buttons); });
+
+  $(".b.1").click(function(){ activate_t(".d.1", buttons_t); });
+  $(".b.2").click(function(){ activate_t(".d.2", buttons_t); });
+  $(".b.3").click(function(){ activate_t(".d.3", buttons_t); });
+  $(".b.4").click(function(){ activate_t(".d.4", buttons_t); });
+
+});
